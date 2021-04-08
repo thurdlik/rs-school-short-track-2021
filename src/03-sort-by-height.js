f4f5204b6f73508203e66782162606b7a3f514b5
Linar <>
2021-04-08 14:33:50 +0300

    feat: implement sort-by-height function

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,20 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      result.push(arr[i]);
+    }
+  }
+  result = result.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      result.splice(i, 0, -1);
+    }
+  }
+  return result;
 }
 
 module.exports = sortByHeight;
