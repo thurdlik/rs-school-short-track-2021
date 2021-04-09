a260c6856907901f3e71612d13ed09e0fe8f2cb6
Linar <>
2021-04-09 19:55:49 +0300

    feat: implement sum-digits function

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,20 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let count = 0;
+  if (n.length === 1) {
+    return n;
+  }
+  let myArr = n.toString().split('');
+  while (myArr.length !== 1) {
+    count = 0;
+    for (let i = 0; i < myArr.length; i++) {
+      count += parseInt(myArr[i], 10);
+    }
+    myArr = count.toString().split('');
+  }
+  return count;
 }
 
 module.exports = getSumOfDigits;
