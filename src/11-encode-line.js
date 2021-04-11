5758457c25f475b94e2dcb1a9556c6329bd8b115
Linar <>
2021-04-11 12:46:50 +0300

    implement encode-line function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  let sum = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      sum += 1;
+    } else {
+      result.push(sum + str[i]);
+      sum = 1;
+    }
+  }
+  let answer = result.join('');
+  answer = answer.replace(/1/gm, '');
+  return answer;
 }
 
 module.exports = encodeLine;
