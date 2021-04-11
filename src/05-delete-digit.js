30fa08a832f0a5f9e5d0322bd145831b84b110d9
Linar <>
2021-04-11 12:12:25 +0300

    implement delete-digit function

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,21 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const result = [];
+  let sum = 0;
+  let str = n.toString();
+  for (let i = 0; i < str.length; i++) {
+    str = str.replace(str[i], '');
+    result.push(parseInt(str, 10));
+    str = n.toString();
+  }
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] > sum) {
+      sum = result[i];
+    }
+  }
+  return sum;
 }
 
 module.exports = deleteDigit;
